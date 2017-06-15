064f2467086ab6558eac790086a8396f1b9154c7
romashka50 <roma.buchuk@gmail.com>
2017-06-15 22:44:36 +0300

    Start real project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-const button = document.createElement('button');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Routes from './router';
+import '../style/materialize.css';
+import '../style/react-range.css';
+import '../style/style.css';
 
-button.innerText = 'Click Me';
-button.onclick = () => {
-  System.import('./image-viewer').then((module) => {
-    module.default();
-  });
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 };
 
-document.body.appendChild(button);
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,20 @@
+// eslint-disable-next-line
+const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const config = {
+module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: 'build/',
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
         test: /\.js$/,
-      },
-      {
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader',
-        }),
-        test: /\.css$/,
-      },
-      {
-        test: /\.(jpe?g|gif|png|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 40000,
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-
-            },
-          },
-        ],
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
     ],
   },
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-  ],
 };
-
-module.exports = config;
