b66756c3e8c169888fb3361e5becf1d01a6813b0
romashka50 <roma.buchuk@gmail.com>
2017-06-16 23:13:37 +0300

    add clean scrip & code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
 // eslint-disable-next-line
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIST = [
+  'faker',
+  'lodash',
+  'react',
+  'react-dom',
+  'react-input-range',
+  'react-redux',
+  'react-router',
+  'redux',
+  'redux-form',
+  'redux-thunk',
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIST,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
@@ -15,6 +32,21 @@ module.exports = {
         use: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ],
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 };
